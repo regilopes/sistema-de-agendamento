256f8a8e79debeb41f9b2af3cf07adc2fd3225d8
Reginaldo Lopes <103508426+regilopes@users.noreply.github.com>
2022-06-08 17:39:16 -0300

    6. Migration usuários

---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+module.exports = {
+   dialect: 'postgres',
+   url: 'postgres://isyshrcq:qczGWKdzGlG4wsCxRi-QOkA2BYurxW7G@fanny.db.elephantsql.com/isyshrcq',
+   define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true
+   }
+}
