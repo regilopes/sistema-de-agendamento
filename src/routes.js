707e121ec050699ef061ecd411aa6b14db542168
Reginaldo Lopes <103508426+regilopes@users.noreply.github.com>
2022-06-09 17:52:35 -0300

    8. Controller usuários

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
-import Database from './database/index'
+import UserController from './app/controllers/UserController';
+import Database from './database/index' //NÃO MEXER
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-   const user = await User.create({
-      name: 'Reginaldo',
-      email: 'reginaldo@email.com',
-      password_hash: '12345',
-   });
-   return res.json(user)
-})
+routes.post('/users', UserController.store)
 
 export default routes;
